7344f531604955d924d90f8093d822c61418e84f
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-05-01 20:48:26 +0400

    invoice detailed update

--- invoices.js
+++ invoices.js
@@ -1,5 +1,18 @@
 "use strict";
-import data from "./data.json";
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "./data.json");
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText);
+      // Access the data object
+      console.log(data);
+    } else {
+      console.error("Failed to load data.json");
+    }
+  }
+};
+xhr.send();
 
 
 
